82389f30aab0817eb048d30088dc32a589ea2405
tndumarevskaya <63363543+tndumarevskaya@users.noreply.github.com>
2020-09-15 16:23:11 +0300

    Update task1.cpp

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,5 +1,5 @@
-﻿#include "task1.h"
-#include <math.h>
+#include "task1.h"
+#include <cmath>
 
 double delta = 0.00000001;
 
@@ -7,14 +7,17 @@ double average(double x, double y)
 {
 	return (x + y) / 2.0;
 }
+
 bool good(double guess, double x)
 {
 	return fabs(guess * guess - x) < delta;
 }
+
 double improve(double guess, double x)
 {
 	return average(guess, x / guess);
 }
+
 double iter(double guess, double x)
 {
 	if (good(guess, x))
@@ -22,6 +25,7 @@ double iter(double guess, double x)
 	else
 		return iter(improve(guess, x), x);
 }
+
 double calc(double arg)
 {
 	return iter(1.0, arg);
