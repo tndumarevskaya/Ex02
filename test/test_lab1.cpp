143f52b8054bb5ead99244103534f3e49c9cb262
tndumarevskaya <63363543+tndumarevskaya@users.noreply.github.com>
2020-09-15 12:32:04 +0300

    Add files via upload

--- test/test_lab1.cpp
+++ test/test_lab1.cpp
@@ -1,5 +1,52 @@
 #include "task1.h"
-
 #include "gtest/gtest.h"
 
+TEST(task1, test1)
+{
+	ASSERt_DOUBLE_EQ(1.0, calc(1.0));
+}
 
+TEST(task1, test1)
+{
+	ASSER_DOUBLE_EQ(1.414214, calc(2.0));
+}
+
+TEST(task1, test1)
+{
+	ASSER_DOUBLE_EQ(1.732051, calc(3.0));
+}
+
+TEST(task1, test1)
+{
+	ASSER_DOUBLE_EQ(2.236068, calc(5.0));
+}
+
+TEST(task1, test1)
+{
+	ASSER_DOUBLE_EQ(2.449490, calc(6.0));
+}
+
+TEST(task1, test1)
+{
+	ASSER_DOUBLE_EQ(2.645751, calc(7.0));
+}
+
+TEST(task1, test1)
+{
+	ASSER_DOUBLE_EQ(2.828427, calc(8.0));
+}
+
+TEST(task1, test1)
+{
+	ASSER_DOUBLE_EQ(3.464102, calc(12.0));
+}
+
+TEST(task1, test1)
+{
+	ASSER_DOUBLE_EQ(3.872983, calc(15.0));
+}
+
+TEST(task1, test1)
+{
+	ASSER_DOUBLE_EQ(4.898979, calc(24.0));
+}
