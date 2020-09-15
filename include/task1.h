3605c3c9c9b92185c992929ea30c89b3f9954f0a
tndumarevskaya <63363543+tndumarevskaya@users.noreply.github.com>
2020-09-15 15:40:52 +0300

    Add files via upload

---
+++ include/task1.h
@@ -0,0 +1,10 @@
+#ifndef TASK1_h
+#define TASK1_h
+
+double average(double x, double y);
+bool good(double guess, double x);
+double improve(double guess, double x);
+double iter(double guess, double x);
+double calc(double arg);
+
+#endif
